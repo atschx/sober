53418c12c0c12880f108359eba7ac5331670a2ad
Albert Sun <atschx@gmail.com>
2015-12-08 19:59:02 +0800

    add new file

--- Sober/ViewController.swift
+++ Sober/ViewController.swift
@@ -7,22 +7,34 @@
 //
 
 import UIKit
+import AdSupport
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //var advertisingIdentifier : NSUUID!{ get }
+        
+        //let UUID = NSUUID.UUID().UUIDString
+        
+        let UUID = UIDevice.currentDevice().identifierForVendor!.UUIDString
+      
+        print("\(UUID)")
+        
         let webview = UIWebView(frame:self.view.bounds)
         webview.bounds=self.view.bounds
+        
         //远程网页
         webview.loadRequest(NSURLRequest(URL: NSURL(string:"http://wx.itry.com/itry/xb_verify?param=F87C6E8D83DD20D76C6FED0F5623672C&idfa=C84F8C7E-44C2-4BD7-8916-499199B8833A&msg=100&ver=1.19&binding=29_1")!))
+        
+        webview.loadRequest(NSURLRequest(URL: NSURL(string:"http://baidu.com")!))
         //项目资源
 //        webview.loadRequest(
 //            NSURLRequest(
 //                URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("index", ofType: "html")!)!
 //            )
 //        )
-        
         self.view.addSubview(webview)
         // Do any additional setup after loading the view, typically from a nib.
     }
